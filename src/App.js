d82ace37e6219ad61f660bfd53db303296a83059
nickscaglione <nicholas.a.scaglione@gmail.com>
2016-11-23 10:38:09 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
 import logo from './logo.svg';
 import './App.css';
+import ShoppingList from './ShoppingList';
 
 class App extends Component {
   render() {
@@ -8,6 +10,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <h2>Welcome to React Shopping List</h2>
+          <img src={logo} />
         </div>
         <ShoppingList items={this.props.shoppingListItems} />
       </div>
@@ -15,4 +18,11 @@ class App extends Component {
   }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {shoppingListItems: state.shoppingListItems}
+}
+
+const connector = connect(mapStateToProps)
+const connectedComponent = connector(App)
+
+export default connectedComponent;
